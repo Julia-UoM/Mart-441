31e397f051c5fac4a83b410f968a5d4b9aa641c7
Julia-UoM <jp225661@umconnect.umt.edu>
2026-01-30 15:36:03 -0700

    Create script.js

---
+++ HW3/script.js
@@ -0,0 +1,14 @@
+/*
+Author: Julia
+Date: Jan 22, 2026
+Purpose: Practice using comments and console.log
+*/
+
+// Print a greeting
+console.log("Hello, world!");
+
+// Print a number
+console.log(42);
+
+// Print a sentence
+console.log("JavaScript is running!");
